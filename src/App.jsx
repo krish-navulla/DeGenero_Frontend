77b96629a33165f916d4cf1599adff8a5ce08bca
krish-navulla <navullavamshi2423@gmail.com>
2024-11-24 15:54:24 -0500

    initial Commit Working

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hero from './components/Hero'
+const App = () => {
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      {/* <h1 className='text-5xl text-violet-300 font-bold'>Welcome to De-Genero</h1> */}
+      <Hero />
+    </main>
+  )
+}
+
+export default App
