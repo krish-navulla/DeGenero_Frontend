e6a08b94232ac0d5b15ef356b4738fb96c4f574a
krish-navulla <navullavamshi2423@gmail.com>
2024-11-25 00:37:03 -0500

    Working customized

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,16 +1,136 @@
+import { useGSAP } from '@gsap/react'
 import React from 'react'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
+import AnimatedTitle from './AnimatedTitle'
 
+gsap.registerPlugin(ScrollTrigger)
 const About = () => {
+
+    useGSAP(() => {
+        const clipAnimation = gsap.timeline({
+            scrollTrigger: {
+                trigger: '#clip',
+                start: 'center center',
+                end: '+=800 center',
+                scrub: 0.5,
+                // pin spacing
+                pin: true,
+                pinSpacing: true
+            }
+        })
+
+        clipAnimation.to('.mask-clip-path', {
+            width:'50vw',
+            height:'50vh',
+            borderRadius:0,
+        })
+
+
+            // // Animate each feature alternately
+            // gsap.fromTo(
+            //     "#feature1", 
+            //     { x: "100%", opacity: 0 }, // Start off-screen to the right
+            //     {
+            //         x: "40%", // Move to center
+            //         opacity: 1, 
+            //         duration: 4,
+            //         ease: "power2.out",
+            //         scrollTrigger: {
+            //             trigger: "#feature1",
+            //             start: "top 80%", // Start animation when the feature enters the viewport
+            //             end: "top 50%", 
+            //             scrub: true
+            //         }
+            //     }
+            // );
+
+            // gsap.fromTo(
+            //     "#feature2",
+            //     { x: "-100%", opacity: 0 }, // Start off-screen to the left
+            //     {
+            //         x: "0%",
+            //         opacity: 1,
+            //         duration: 4,
+            //         ease: "power2.out",
+            //         scrollTrigger: {
+            //             trigger: "#feature2",
+            //             start: "top 80%",
+            //             end: "top 50%",
+            //             scrub: true
+            //         }
+            //     }
+            // );
+
+            // gsap.fromTo(
+            //     "#feature3",
+            //     { x: "100%", opacity: 0 }, // Start off-screen to the right
+            //     {
+            //         x: "40%",
+            //         opacity: 1,
+            //         duration: 4,
+            //         ease: "power2.out",
+            //         scrollTrigger: {
+            //             trigger: "#feature3",
+            //             start: "top 80%",
+            //             end: "top 50%",
+            //             scrub: true
+            //         }
+            //     }
+            // );
+
+            // gsap.fromTo(
+            //     "#feature4",
+            //     { x: "-100%", opacity: 0 }, // Start off-screen to the left
+            //     {
+            //         x: "0%",
+            //         opacity: 1,
+            //         duration: 4,
+            //         ease: "power2.out",
+            //         scrollTrigger: {
+            //             trigger: "#feature4",
+            //             start: "top 80%",
+            //             end: "top 50%",
+            //             scrub: true
+            //         }
+            //     }
+            // );
+
+    })
+
     return (
     <div  id="about" className="min-h-screen w-screen">
         <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
             <h2 className="font-general text-sm uppercase md:text-[1rem]">
             Why Choose De Genero for Your Reporting Needs?
             </h2>
-            <div className="mt-5 text-center text-2xl uppercase leading-[0.8] md:text-[4rem]">
-            <b>T</b>imely, <b>A</b>ccurate data with customizable reports to build <b>T</b>rust and <b>a</b>chieve goals efficiently.
+            <AnimatedTitle title= "<b>T</b>imely, <b>A</b>ccurate data with customizable reports"
+            containerClass="mt-5 !text-black text-center"/>
+
+
+            <div className="about-subtext">
+                <p>De Genero simplifies social impact reporting with innovative, real-time solutions, empowering organizations to drive transparency, efficiency, and meaningful change.
+                </p>
             </div>
         </div>
+        <div className="h-dvh w-screen" id="clip">
+            <div className="mask-clip-path about-image">
+                <img 
+                src="public\img\about.png"
+                alt="Background"
+                className="absolute left-0 top-0 size-full object-cover"
+                />
+            </div>
+        </div>
+        {/* <div className="h-dvh w-screen mt-36">
+        <div class="features">
+            <div class="feature" id="feature1">Feature 1 Content</div>
+            <div class="feature" id="feature2">Feature 2 Content</div>
+            <div class="feature" id="feature3">Feature 3 Content</div>
+            <div class="feature" id="feature4">Feature 4 Content</div>
+        </div>
+        </div> */}
+
     </div>
     )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
     const [isLoading, setIsLoading] = useState(true);
     const [loadedVideos, setLoadedVideos] = useState(0);
 
-    const totalVideos = 4;
+    const totalVideos = 1;
     const nextVideoRef = useRef(null)
 
     const upcomingVideoIndex = (currentIndex % totalVideos) + 1
@@ -32,33 +32,32 @@ const Hero = () => {
         }
     })
 
-    useGSAP(() => {
-        if (hasClicked) {
-            gsap.set('#next-video', {visibility: 'visible'});
-            gsap.to('#next-video', {
-                transformOrigin : 'center center',
-                scale: 1,
-                width:'100%',
-                height: '100%',
-                duration: 1,
-                ease: 'power1.inOut',
-                onStart: () => nextVideoRef.current.play(),
-            });
+    // useGSAP(() => {
+    //     if (hasClicked) {
+    //         gsap.set('#next-video', {visibility: 'visible'});
+    //         gsap.to('#next-video', {
+    //             transformOrigin : 'center center',
+    //             scale: 1,
+    //             width:'100%',
+    //             height: '100%',
+    //             duration: 1,
+    //             ease: 'power1.inOut',
+    //             onStart: () => nextVideoRef.current.play(),
+    //         });
 
-            gsap.from('#current-video', {
-                transformOrigin: 'center center',
-                scale: 0,
-                duration: 1.5,
-                ease: 'power1.inOut',
+    //         gsap.from('#current-video', {
+    //             transformOrigin: 'center center',
+    //             scale: 0,
+    //             duration: 1.5,
+    //             ease: 'power1.inOut',
                 
-            })
-        }
-    }, {dependencies: [currentIndex], revertOnUpdate: true})
+    //         })
+    //     }
+    // }, {dependencies: [currentIndex], revertOnUpdate: true})
 
     useGSAP( ()=>{
         gsap.set('#video-frame', {
-            // clipPath: 'polygon(50% 0%, 80% 10%, 100% 35%, 100% 70%, 80% 90%, 50% 100%, 20% 90%, 0% 70%, 0% 35%, 20% 10%)',
-            clipPath : 'polygon(14% 0%, 72% 0%, 90% 90%, 0% 100%)',
+            clipPath : 'polygon(0% 0%, 80% 0%, 80% 100%, 0% 100%)',
             bordorRadius: '0 0 40% 10%'
         })
 
@@ -76,6 +75,8 @@ const Hero = () => {
 
 
     })
+   
+    
 
     const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
 
@@ -94,7 +95,7 @@ const Hero = () => {
 
             <div id="video-frame" className='relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75'>
                 <div>
-                <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
+                {/* <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
                     <div onClick={handleMiniVdClick} 
                     className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100">
                         <video ref={nextVideoRef}
@@ -107,9 +108,9 @@ const Hero = () => {
                         />
                     </div>
                 
-                </div>
+                </div> */}
 
-                <video 
+                {/* <video 
                     ref={nextVideoRef}
                     src={getVideoSrc(currentIndex)}
                     loop
@@ -117,26 +118,27 @@ const Hero = () => {
                     id="next-video"
                     className="absolute-center invisible absolute z-20 size-64 object-cover object-center"   
                     onLoadedData={handleVideoLoad}                 
-                />
+                /> */}
                 <video 
-                src={getVideoSrc(currentIndex=== totalVideos - 1 ? 1: currentIndex)}
-                // autoPlay
+                // src={getVideoSrc(currentIndex === totalVideos - 1 ? 1: currentIndex)}
+                src={getVideoSrc(1)}
+                autoPlay
                 loop
                 muted
                 className="absolute left-0 top-0 size-full object-cover object-center"
                 onLoadedData={handleVideoLoad}
                 />
             </div>
-            <h1 className="special-font hero-heading-small absolute bottom-5 right-5 z-40 text-blue-75">
+            {/* <h1 className="special-font hero-heading-small absolute bottom-5 right-5 z-40 text-blue-75">
                     Imp<b>A</b>ct
-                </h1>
+                </h1> */}
 
             <div className="absolute left-0 top-0 z-40 size-full">
                 <div className="mt-24 px-5 sm:px-10">
                     <h1 className="special-font hero-heading text-blue-100">
                         De Gener<b>o</b></h1>
                     <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
-                    Amplify your impact with real-time reporting</p> 
+                    Amplify your impact with real-time reporting</p>
                 
                     <Button 
                     id="watch-trailer" title="Start your Impact Journey" leftIcon={<TiLocationArrow />}
@@ -148,9 +150,19 @@ const Hero = () => {
 
             </div>
             <h1 className="special-font hero-heading-small absolute bottom-5 right-5 text-black">
-                    Imp<b>A</b>ct
-                </h1>
+                <span className="block font-bold">Impact</span>
+                <span className="block font-bold">Trust</span>
+                <span className="block font-bold">Insight</span>
+            </h1>
 
+
+            {/* <h1 className="special-font hero-heading-small absolute bottom-5 right-5 text-black">
+                <b>E</b>fficiency
+            </h1>
+            <h1 className="special-font hero-heading-small absolute bottom-5 right-5 text-black">
+                    <b>E</b>mpowerment
+                </h1> */}
+            
         </div>
         
     )
